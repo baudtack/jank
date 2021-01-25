4faed48c9c9c5009740222a2733ca54b73eba826
David Kerschner <dkerschner@hcoop.net>
2021-01-25 15:39:11 -0800

    working mix with test

--- nock/math.js
+++ nock/math.js
@@ -26,6 +26,45 @@ function met32(a, b) {
 }
 
 function mix(a, b) {
+    a = mint(a);
+    b = mint(b);
+
+    if(a instanceof JSBI || b instanceof JSBI) {
+        return mint(JSBI.bitwiseXor(JSBI.BigInt(a),
+                                    JSBI.BigInt(b)));
+    } else if (a > 0xffffffff) {
+        if (b > 0xffffffff) {
+            var topa = top(a);
+            var bota = bot(a);
+            var topb = top(b);
+            var botb = bot(b);
+
+            var t = topa ^ topb;
+            var b = bota ^ botb;
+
+            return (t * 0x100000000) + b;
+        } else {
+            var topa = top(a);
+            var bota = bot(a);
+
+            return (topa * 0x100000000) + (bota ^ b);
+        }
+    } else if (b > 0xffffffff) {
+        var topb = top(b);
+        var botb = bot(b);
+
+        return (topb * 0x100000000) + (botb ^ a);
+    } else {
+        return mint(a ^ b);
+    }
+}
+
+function top(a) {
+    return a / 0x100000000;
+}
+
+function bot(a) {
+    return a % 0x100000000;
 }
 
 function mint(a) {
